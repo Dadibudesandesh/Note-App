7ec397e689ce94c9270a909d6c97b173c5b706f8
Sandesh Dadibude <129705613+Dadibudesandesh@users.noreply.github.com>
2026-05-03 03:18:55 +0530

    Update API endpoints for note operations

--- note-frontend/src/api.js
+++ note-frontend/src/api.js
@@ -5,6 +5,6 @@ const URL="https://note-app-production-6540.up.railway.app/api/notes";
 export const createNote =(note)=>axios.post(`${URL}/addNote`,note);
 export const getAllNotes =()=>axios.get(`${URL}/getAll`);
 export const getNoteById =(id)=>axios.get(`${URL}/getNote/${id}`);
-export const updateNote =(id,note)=>axios.put(`${URL}/updateNote/${id}`,note);
-export const deleteNote =(id)=>axios.delete(`${URL}/deleteNote/${id}`);
+export const updateNote = (id, note) =>axios.put(`${URL}/update/${id}`, note);
+export const deleteNote = (id) =>axios.delete(`${URL}/delete/${id}`);
 export const searchNote =(string)=>axios.get(`${URL}/search?keyword=${string}`);
