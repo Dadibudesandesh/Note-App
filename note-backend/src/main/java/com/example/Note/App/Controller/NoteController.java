3c8af2d062fce10bf12983a90220992c77cc4556
Sandesh Dadibude <129705613+Dadibudesandesh@users.noreply.github.com>
2026-05-03 02:37:56 +0530

    Add files via upload

---
+++ note-backend/src/main/java/com/example/Note/App/Controller/NoteController.java
@@ -0,0 +1,48 @@
+package com.example.Note.App.Controller;
+
+import com.example.Note.App.Entity.Note;
+import com.example.Note.App.Service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+    @Autowired
+    NoteService noteService;
+
+    @GetMapping("/getAll")
+    public List<Note> getAll(){
+        return noteService.getAll();
+    }
+
+    @GetMapping("/getNote/{id}")
+    public Note getOne(@PathVariable int id){
+        return  noteService.getOne(id);
+    }
+
+    @GetMapping("/search")
+    public List<Note> searchNote(@RequestParam String keyword){
+        return noteService.searchNote(keyword);
+    }
+
+    @PostMapping("/addNote")
+    public Note addNote(@RequestBody Note note){
+        return noteService.addNote(note);
+    }
+
+    @PutMapping("/update/{id}")
+    public Note updateNote(@RequestBody Note note,@PathVariable int id){
+        return noteService.update(note,id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Note remove(@PathVariable int id){
+        return noteService.delete(id);
+    }
+
+
+}
